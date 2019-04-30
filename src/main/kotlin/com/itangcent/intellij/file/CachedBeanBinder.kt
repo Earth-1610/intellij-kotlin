2284f0d21851b3a6af1bac040282933d7b909118
tangcent <sweettom@foxmail.com>
2019-04-30 21:57:55 +0800

    provide lazy BeanBinder

---
+++ src/main/kotlin/com/itangcent/intellij/file/CachedBeanBinder.kt
@@ -0,0 +1,41 @@
+package com.itangcent.intellij.file
+
+import com.itangcent.intellij.context.ActionContext
+
+/**
+ * bind a file which content present a bean
+ */
+class CachedBeanBinder<T : Any> : BeanBinder<T> {
+
+    private var delegate: BeanBinder<T>
+
+    @Volatile
+    var cache: T? = null
+
+    constructor(delegate: BeanBinder<T>) {
+        this.delegate = delegate
+    }
+
+    override fun read(): T {
+        if (cache == null) {
+            cache = delegate.read()
+        }
+        return cache as T
+    }
+
+    override fun save(t: T) {
+        cache = t
+        val context = ActionContext.getContext()
+        if (context == null) {
+            delegate.save(t)
+        } else {
+            context.runAsync {
+                delegate.save(t)
+            }
+        }
+    }
+}
+
+fun <T : Any> BeanBinder<T>.lazy(): CachedBeanBinder<T> {
+    return CachedBeanBinder(this)
+}
