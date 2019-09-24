c8b17b099eaac9a8545391177036bc363519044d
tangcent <sweettom@foxmail.com>
2019-09-25 07:40:48 +0800

    converter class type multiple times

--- intellij-idea/src/main/kotlin/com/itangcent/intellij/psi/AbstractClassRuleConfig.kt
+++ intellij-idea/src/main/kotlin/com/itangcent/intellij/psi/AbstractClassRuleConfig.kt
@@ -7,9 +7,14 @@ abstract class AbstractClassRuleConfig : ClassRuleConfig {
     private var convertCache: MutableMap<String, String> = LinkedHashMap()
 
     override fun tryConvert(cls: String): String? {
-
         return convertCache.computeIfAbsentSafely(cls) {
-            convertByRule(it)
+            var origin = cls;
+            var convertByRule = convertByRule(it)
+            while (convertByRule != origin) {
+                origin = convertByRule
+                convertByRule = convertByRule(origin)
+            }
+            return@computeIfAbsentSafely convertByRule
         }
     }
 
