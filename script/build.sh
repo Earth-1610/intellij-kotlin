4f46d3d06cab6e2659886bf21911b0fc500f79c1
tangcent <sweettom@foxmail.com>
2019-09-28 09:16:36 +0800

    fix script

--- script/build.sh
+++ script/build.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
 
-cd ..
-gradle clean build
+SOURCE="$0"
+while [[ -h "$SOURCE"  ]]; do # resolve $SOURCE until the file is no longer a symlink
+    scriptDir="$( cd -P "$( dirname "$SOURCE"  )" && pwd  )"
+    SOURCE="$(readlink "$SOURCE")"
+    [[ ${SOURCE} != /*  ]] && SOURCE="$scriptDir/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+scriptDir="$( cd -P "$( dirname "$SOURCE"  )" && pwd  )"
+basedir=${scriptDir%/*}
+echo "baseDir:"${basedir}
+cd ${basedir}
+
+./gradlew  clean build
