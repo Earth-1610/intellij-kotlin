0fc08547451667e65afc87206fa0d7302cf2c39a
tangcent <sweettom@foxmail.com>
2019-12-18 07:52:54 +0800

    opti: read with special charset

--- commons/src/main/kotlin/com/itangcent/common/files/FileWrap.kt
+++ commons/src/main/kotlin/com/itangcent/common/files/FileWrap.kt
@@ -11,7 +11,7 @@ import java.io.IOException
 class FileWrap(private val root: String, var file: File) {
 
     fun content(): String {
-        return FileUtils.read(file)
+        return FileUtils.read(file) ?: ""
     }
 
     fun path(): String {
