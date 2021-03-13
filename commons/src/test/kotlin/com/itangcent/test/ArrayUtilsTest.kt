04bf89b047b818e556a2f7021ae92a5e5aa4275c
tangcent <pentatangcent@gmail.com>
2021-03-13 21:48:12 +0800

    chore: add test case for [ArrayUtils]

---
+++ commons/src/test/kotlin/com/itangcent/test/ArrayUtilsTest.kt
@@ -0,0 +1,40 @@
+package com.itangcent.test
+
+import com.itangcent.common.utils.ArrayUtils
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+
+/**
+ * Test case for [ArrayUtils]
+ */
+class ArrayUtilsTest {
+
+    @Test
+    fun testContains() {
+        assertFalse(ArrayUtils.contains(emptyArray<String>(), ""))
+        assertFalse(ArrayUtils.contains(emptyArray<String>(), null))
+
+        assertFalse(ArrayUtils.contains(arrayOf("a", "b"), ""))
+        assertFalse(ArrayUtils.contains(arrayOf("a", "b"), null))
+        assertTrue(ArrayUtils.contains(arrayOf("a", "b"), "a"))
+        assertFalse(ArrayUtils.contains(arrayOf("a", "b"), "c"))
+    }
+
+    @Test
+    fun testIndexOf() {
+        assertEquals(-1, ArrayUtils.indexOf(emptyArray<String>(), ""))
+        assertEquals(-1, ArrayUtils.indexOf(emptyArray<String>(), null))
+
+        assertEquals(-1, ArrayUtils.indexOf(arrayOf("a", "b"), ""))
+        assertEquals(-1, ArrayUtils.indexOf(arrayOf("a", "b"), null))
+        assertEquals(0, ArrayUtils.indexOf(arrayOf("a", "b"), "a"))
+        assertEquals(-1, ArrayUtils.indexOf(arrayOf("a", "b"), "c"))
+
+        assertEquals(-1, ArrayUtils.indexOf(arrayOf("a", "b", "c"), "a", 1))
+        assertEquals(1, ArrayUtils.indexOf(arrayOf("a", "b", "c"), "b", 1))
+    }
+
+}
