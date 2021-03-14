59a676a1269d95c23e16faee750cfe1f3dcfae2a
tangcent <pentatangcent@gmail.com>
2021-03-14 11:24:37 +0800

    chore: update test case of [TimeSpanUtilsTest]

--- commons/src/test/kotlin/com/itangcent/test/TimeSpanUtilsTest.kt
+++ commons/src/test/kotlin/com/itangcent/test/TimeSpanUtilsTest.kt
@@ -1,31 +1,51 @@
 package com.itangcent.test
 
-import com.itangcent.common.utils.IDUtils
 import com.itangcent.common.utils.TimeSpanUtils
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.ValueSource
 import java.util.concurrent.TimeUnit
 
 
 /**
- * Test case of [IDUtils]
+ * Test case of [TimeSpanUtils]
  */
 class TimeSpanUtilsTest {
 
     @ParameterizedTest
     @CsvSource(
-        "10,10ms", "3610000,1h10s", "720000,0.2h",
+        "0,''", "10,10ms", "3610000,1h10s", "720000,0.2h",
         "720000000,200h", "1800000000,20d20h", "700000000,700000s", "4320000,0.05d"
+        , nullValues = ["null"]
     )
     fun testParse(ms: Long, str: String) {
         assertEquals(ms, TimeSpanUtils.parse(str))
     }
 
     @ParameterizedTest
+    @ValueSource(strings = ["1m2a", "99x"])
+    fun testParseIllegal(str: String) {
+        assertThrows<IllegalArgumentException> {
+            TimeSpanUtils.parse(str)
+        }
+    }
+
+    @ParameterizedTest
     @CsvSource(
-        "0.05,180000ms", "1.1,1h6m", "0.2,0.2h",
+        "10,MILLISECONDS,10ms", "3610,SECONDS,1h10s", "12,MINUTES,0.2h",
+        "12000,MINUTES,200h", "500,HOURS,20d20h", "700000,SECONDS,700000s", "4320,SECONDS,0.05d"
+    )
+    fun testParse(ms: Long, unit: TimeUnit, str: String) {
+        assertEquals(ms, TimeSpanUtils.parse(str, unit))
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "0.0,''", "0.05,180000ms", "1.1,1h6m", "0.2,0.2h",
         "200,200h", "500,20d20h", "1000,3600000s", "1.2,0.05d"
+        , nullValues = ["null"]
     )
     fun testPrecisionParse(ms: Double, str: String) {
         assertEquals(ms, TimeSpanUtils.precisionParse(str, TimeUnit.HOURS))
@@ -33,7 +53,7 @@ class TimeSpanUtilsTest {
 
     @ParameterizedTest
     @CsvSource(
-        "10,10ms", "3610000,1h10s", "720000,12m",
+        "0,0ms", "10,10ms", "3610000,1h10s", "720000,12m",
         "720000000,8d8h", "1800000000,20d20h", "700000000,8d2h26m40s", "4320000,1h12m"
     )
     fun testPretty(ms: Long, str: String) {
