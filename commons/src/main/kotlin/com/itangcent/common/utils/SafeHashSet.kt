ae009fedc1d0e7006b5c8abc11449f227e1eb5c4
tangcent <pentatangcent@gmail.com>
2021-06-08 20:33:22 +0800

    fix: Unwrapper.unwrapped

--- commons/src/main/kotlin/com/itangcent/common/utils/SafeHashSet.kt
+++ commons/src/main/kotlin/com/itangcent/common/utils/SafeHashSet.kt
@@ -2,9 +2,9 @@ package com.itangcent.common.utils
 
 import java.util.*
 
-class SafeHashSet<E> : HashSet<E>() {
+open class SafeHashSet<E> : HashSet<E>() {
 
-    private val failedElements = LinkedList<E>()
+    protected val failedElements = LinkedList<E>()
 
     override fun add(element: E): Boolean {
         try {
@@ -21,4 +21,49 @@ class SafeHashSet<E> : HashSet<E>() {
             }
         }
     }
+}
+
+class ReentrantSafeHashSet<E>(private val reentrant: Int) : SafeHashSet<CntElement<E>>() {
+
+    fun addElement(element: E): Boolean {
+        return add(CntElement(element))
+    }
+
+    override fun add(element: CntElement<E>): Boolean {
+        if (super.add(element)) {
+            return true
+        }
+        return (find { it == element } ?: failedElements.find { it == element })!!.incr() <= reentrant
+    }
+}
+
+class CntElement<E>(val e: E) {
+    /**
+     * 1 be init
+     */
+    var cnt = 1
+
+    fun incr(): Int {
+        return ++cnt
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as CntElement<*>
+
+        if (e != other.e) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return e?.hashCode() ?: 0
+    }
+
+    override fun toString(): String {
+        return e.toString()
+    }
+
 }
---
+++ intellij-idea/src/test/kotlin/com/itangcent/intellij/psi/UnwrapperTest.kt
@@ -0,0 +1,103 @@
+package com.itangcent.intellij.psi
+
+import com.itangcent.mock.AdvancedContextTest
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNull
+import kotlin.test.assertTrue
+
+/**
+ * Test case of [Unwrapper]
+ */
+internal class UnwrapperTest : AdvancedContextTest() {
+
+    @Test
+    fun testUnwrapped() {
+        assertNull(Unwrapper().unwrapped(null) {})
+        assertEquals(1, Unwrapper().unwrapped(1) {})
+        assertEquals("s", Unwrapper().unwrapped("s") {})
+        val node = hashMapOf<String, Any?>("x" to 1, "y" to 1, "s1" to "", "s2" to "", "null" to null, "msg" to "root")
+        node["sub"] = Delay(node)
+
+        checkEqual(hashMapOf(
+            "x" to 1, "y" to 1, "s1" to "", "s2" to "", "null" to null, "msg" to "root",
+            "sub" to hashMapOf(
+                "x" to 1, "y" to 1, "s1" to "", "s2" to "", "null" to null, "msg" to "root",
+                "sub" to Any()
+            )
+        ), Unwrapper().unwrapped(node) {} as Map<*, *>)
+
+        checkEqual(hashMapOf(
+            "x" to 1, "y" to 1, "s1" to "", "s2" to "", "null" to null, "msg" to "root",
+            "sub" to hashMapOf(
+                "x" to 1, "y" to 1, "s1" to "", "s2" to "", "null" to null, "msg" to "root",
+                "sub" to Any()
+            )
+        ), Unwrapper().unwrapped(Delay(node)) {})
+
+        checkEqual(listOf(
+            hashMapOf(
+                "x" to 1, "y" to 1, "s1" to "", "s2" to "", "null" to null, "msg" to "root",
+                "sub" to hashMapOf(
+                    "x" to 1, "y" to 1, "s1" to "", "s2" to "", "null" to null, "msg" to "root",
+                    "sub" to Any()
+                )
+            )
+        ), Unwrapper().unwrapped(listOf(Delay(node))) {})
+    }
+
+    private fun checkEqual(any: Any?, any2: Any?) {
+        if (any == null) {
+            assertNull(any2)
+            return
+        }
+
+        assertTrue(any2 != null)
+
+        if (any::class == Any::class) {
+            assertTrue(any2::class == Any::class)
+            return
+        }
+
+        if (any is Map<*, *>) {
+            assertTrue(any2 is Map<*, *>)
+            checkMapEqual(any, any2)
+            return
+        }
+
+        if (any is List<*>) {
+            assertTrue(any2 is List<*>)
+            checkListEqual(any, any2)
+            return
+        }
+
+        if (any is Array<*>) {
+            assertTrue(any2 is Array<*>)
+            checkArrayEqual(any, any2)
+            return
+        }
+
+        assertEquals(any, any2)
+    }
+
+    private fun checkMapEqual(map: Map<*, *>, map2: Map<*, *>) {
+        assertEquals(map.keys, map2.keys)
+        for (entry in map.entries) {
+            checkEqual(entry.value, map2[entry.key])
+        }
+    }
+
+    private fun checkListEqual(list: List<*>, list2: List<*>) {
+        assertEquals(list.size, list2.size)
+        for ((index, any) in list.withIndex()) {
+            checkEqual(any, list2[index])
+        }
+    }
+
+    private fun checkArrayEqual(arr: Array<*>, arr2: Array<*>) {
+        assertEquals(arr.size, arr2.size)
+        for ((index, any) in arr.withIndex()) {
+            checkEqual(any, arr2[index])
+        }
+    }
+}
