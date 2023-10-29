52649569b6d0ed026f9fa694f5d97113b6826eec
tangcent <pentatangcent@gmail.com>
2023-10-30 07:53:27 +0800

    feat: Parse the document from @param on the record class (#196)

--- intellij-jvm/src/main/kotlin/com/itangcent/intellij/jvm/feature/RecordDocHelper.kt
+++ intellij-jvm/src/main/kotlin/com/itangcent/intellij/jvm/feature/RecordDocHelper.kt
@@ -3,12 +3,22 @@ package com.itangcent.intellij.jvm.feature
 import com.google.inject.Singleton
 import com.intellij.psi.JavaTokenType
 import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiField
 import com.intellij.psi.PsiJavaToken
 import com.intellij.psi.impl.light.LightRecordField
+import com.itangcent.common.utils.appendln
 import com.itangcent.intellij.jvm.standard.StandardDocHelper
 
 @Singleton
 class RecordDocHelper : StandardDocHelper() {
+
+    override fun getAttrOfField(field: PsiField): String? {
+        if (field !is LightRecordField) {
+            return null
+        }
+        val docFromClassParams = findDocsByTagAndName(field.containingClass, "param", field.name)
+        return docFromClassParams.appendln(super.getAttrOfField(field))
+    }
 
     override fun getEolComment(psiElement: PsiElement): String? {
         if (psiElement !is LightRecordField) {
