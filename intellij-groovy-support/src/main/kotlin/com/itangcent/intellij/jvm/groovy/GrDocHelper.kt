796425cc94d020d5f4cb37264ae34366eab70fbe
tangcent <pentatangcent@gmail.com>
2023-11-05 21:24:39 +0800

    fix: Fix issue with discarding field documentation when configuring 'field.doc' rule (#200)

--- intellij-groovy-support/src/main/kotlin/com/itangcent/intellij/jvm/groovy/GrDocHelper.kt
+++ intellij-groovy-support/src/main/kotlin/com/itangcent/intellij/jvm/groovy/GrDocHelper.kt
@@ -3,21 +3,20 @@ package com.itangcent.intellij.jvm.groovy
 import com.google.inject.Singleton
 import com.intellij.psi.PsiComment
 import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiField
 import com.intellij.psi.PsiWhiteSpace
 import com.intellij.psi.impl.source.tree.LeafPsiElement
+import com.intellij.psi.javadoc.PsiDocComment
 import com.itangcent.intellij.jvm.standard.BLOCK_COMMENT_REGEX
 import com.itangcent.intellij.jvm.standard.COMMENT_PREFIX
 import com.itangcent.intellij.jvm.standard.StandardDocHelper
 import org.jetbrains.plugins.groovy.lang.psi.GroovyElementTypes
-import org.jetbrains.plugins.groovy.lang.psi.api.statements.GrField
 
 @Singleton
 class GrDocHelper : StandardDocHelper() {
 
     override fun getEolComment(psiElement: PsiElement): String? {
-        if (psiElement !is GrField) {
-            return null
-        }
+        GrPsiUtils.assertGrPsiInst(psiElement)
 
         psiElement.parent.prevSiblings()
             .findBlockComment()
@@ -32,6 +31,48 @@ class GrDocHelper : StandardDocHelper() {
             }
 
         return null
+    }
+
+    override fun hasTag(psiElement: PsiElement?, tag: String?): Boolean {
+        throw NotImplementedError()
+    }
+
+    override fun findDocByTag(psiElement: PsiElement?, tag: String?): String? {
+        throw NotImplementedError()
+    }
+
+    override fun findDocsByTag(psiElement: PsiElement?, tag: String?): List<String>? {
+        throw NotImplementedError()
+    }
+
+    override fun findDocsByTagAndName(psiElement: PsiElement?, tag: String, name: String): String? {
+        throw NotImplementedError()
+    }
+
+    override fun getAttrOfDocComment(psiElement: PsiElement?): String? {
+        if (psiElement == null) {
+            return null
+        }
+        GrPsiUtils.assertGrPsiInst(psiElement)
+        return super.getAttrOfDocComment(psiElement)
+    }
+
+    override fun getDocCommentContent(docComment: PsiDocComment): String? {
+        GrPsiUtils.assertGrPsiInst(docComment)
+        return super.getDocCommentContent(docComment)
+    }
+
+    override fun getSubTagMapOfDocComment(psiElement: PsiElement?, tag: String): Map<String, String?> {
+        throw NotImplementedError()
+    }
+
+    override fun getTagMapOfDocComment(psiElement: PsiElement?): Map<String, String?> {
+        throw NotImplementedError()
+    }
+
+    override fun getAttrOfField(field: PsiField): String? {
+        GrPsiUtils.assertGrPsiInst(field)
+        return super.getAttrOfField(field)
     }
 
     override fun PsiComment.eolComment(): String? {
