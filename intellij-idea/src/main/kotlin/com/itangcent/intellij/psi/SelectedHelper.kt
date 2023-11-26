bbfeeb95e51cfcafdb72f7408d63682a5807bd69
tangcent <pentatangcent@gmail.com>
2023-11-26 14:45:43 +0800

    fix: Resolve SelectedHelper not functioning correctly in multiple scenarios (#202)

--- intellij-idea/src/main/kotlin/com/itangcent/intellij/psi/SelectedHelper.kt
+++ intellij-idea/src/main/kotlin/com/itangcent/intellij/psi/SelectedHelper.kt
@@ -5,11 +5,7 @@ import com.intellij.ide.projectView.impl.nodes.PsiDirectoryNode
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.actionSystem.DataContext
 import com.intellij.pom.Navigatable
-import com.intellij.psi.PsiClass
-import com.intellij.psi.PsiClassOwner
-import com.intellij.psi.PsiDirectory
-import com.intellij.psi.PsiFile
-import com.itangcent.common.logger.traceWarn
+import com.intellij.psi.*
 import com.itangcent.intellij.context.ActionContext
 import com.itangcent.intellij.logger.Logger
 import com.itangcent.intellij.util.DirFilter
@@ -50,64 +46,60 @@ object SelectedHelper {
 
         private val actionContext = ActionContext.getContext()!!
 
-        private val logger = actionContext.instance(Logger::class)
-
         fun traversal() {
-
             //try to get navigatables
-            try {
-                val navigatables = actionContext.cacheOrCompute(CommonDataKeys.NAVIGATABLE_ARRAY.name) {
-                    actionContext.instance(DataContext::class).getData(CommonDataKeys.NAVIGATABLE_ARRAY)
-                }
-                if (navigatables != null && navigatables.isNotEmpty()) {
-                    val boundary = actionContext.createBoundary()
-                    try {
-                        for (navigatable in navigatables) {
-                            actionContext.runInReadUI {
-                                onNavigatable(navigatable)
-                            }
-                            boundary.waitComplete(false)
-                            Thread.sleep(100)
-                        }
-                    } finally {
-                        boundary.remove()
-                    }
-                    return
-                }
-            } catch (e: Exception) {
-                logger.traceWarn("error handle navigatables", e)
+            val navigatables = actionContext.cacheOrCompute(CommonDataKeys.NAVIGATABLE_ARRAY.name) {
+                actionContext.instance(DataContext::class).getData(CommonDataKeys.NAVIGATABLE_ARRAY)
             }
-
-            //try to get navigatable
-            try {
-                val navigatable = actionContext.cacheOrCompute(CommonDataKeys.NAVIGATABLE.name) {
-                    actionContext.instance(DataContext::class).getData(CommonDataKeys.NAVIGATABLE)
-                }
-                if (navigatable != null) {
-                    actionContext.runInReadUI {
-                        onNavigatable(navigatable)
-                    }
-                    return
-                }
-            } catch (e: Exception) {
-                logger.traceWarn("error handle navigatable", e)
+            if (navigatables != null && navigatables.size > 1) {
+                onNavigatables(navigatables)
+                return
             }
 
             //try to get psiFile
-            try {
-                val psiFile = actionContext.cacheOrCompute(CommonDataKeys.PSI_FILE.name) {
-                    actionContext.instance(DataContext::class).getData(CommonDataKeys.PSI_FILE)
-                }
-                if (psiFile != null) {
-                    if (fileFilter(psiFile)) {
-                        actionContext.runInReadUI {
-                            onFile(psiFile)
-                        }
+            val psiFile = actionContext.cacheOrCompute(CommonDataKeys.PSI_FILE.name) {
+                actionContext.instance(DataContext::class).getData(CommonDataKeys.PSI_FILE)
+            }
+            if (psiFile != null) {
+                if (fileFilter(psiFile)) {
+                    actionContext.runInReadUI {
+                        onFile(psiFile)
                     }
-                    return
                 }
-            } catch (e: Exception) {
-                logger.traceWarn("error handle class", e)
+                return
+            }
+
+            //try to process navigatables
+            if (!navigatables.isNullOrEmpty()) {
+                onNavigatables(navigatables)
+            }
+
+            //try to get navigatable
+            val navigatable = actionContext.cacheOrCompute(CommonDataKeys.NAVIGATABLE.name) {
+                actionContext.instance(DataContext::class).getData(CommonDataKeys.NAVIGATABLE)
+            }
+            if (navigatable != null) {
+                actionContext.runInReadUI {
+                    onNavigatable(navigatable)
+                }
+                return
+            }
+        }
+
+        private fun onNavigatables(navigatables: Array<Navigatable>?) {
+            if (navigatables != null && navigatables.isNotEmpty()) {
+                val boundary = actionContext.createBoundary()
+                try {
+                    for (navigatable in navigatables) {
+                        actionContext.runInReadUI {
+                            onNavigatable(navigatable)
+                        }
+                        boundary.waitComplete(false)
+                        Thread.sleep(100)
+                    }
+                } finally {
+                    boundary.remove()
+                }
             }
         }
 
@@ -127,6 +119,10 @@ object SelectedHelper {
 
                 is PsiDirectoryNode -> {
                     navigatable.element?.value?.let { onDirectory(it) }
+                }
+
+                is PsiMember -> {
+                    navigatable.containingClass?.let { onClass(it) }
                 }
             }
         }
